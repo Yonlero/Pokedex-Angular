27f85e4ec6182dbec64a50a3a8857d97f120293c
Yonlero <abraaobrg150@gmail.com>
2023-08-10 18:00:38 -0300

    Pokedex em angular

---
+++ src/app/shared/poke-list/poke-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { PokeApiService } from 'src/app/service/poke-api.service';
+
+@Component({
+  selector: 'poke-list',
+  templateUrl: './poke-list.component.html',
+  styleUrls: ['./poke-list.component.scss']
+})
+export class PokeListComponent implements OnInit {
+
+  public getAllPokemons: any;
+  private setAllPokemons: any;
+  private apiError: boolean = false;
+
+  constructor(private pokeService: PokeApiService) { }
+
+  ngOnInit(): void {
+    this.pokeService.apiListAllPokemons.subscribe((res: any) => {
+      this.setAllPokemons = res.results;
+      this.getAllPokemons = this.setAllPokemons;
+    },
+      (error: any) => {
+        this.apiError = true;
+      }
+    );
+  }
+
+  public getSearch(value: string) {
+    const filter = this.setAllPokemons.filter(
+      (res: any) => {
+        return !res.name.indexOf(value.toLowerCase())
+      });
+
+    this.getAllPokemons = filter;
+  }
+}
